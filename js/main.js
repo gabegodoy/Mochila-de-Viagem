458da0a11b79669f639fbfe05b931e1a0e45c1ab
Gabriel Godoy <gabe.profe@gmail.com>
2022-06-08 13:21:04 -0300

    adicionar-salvar-editar--itens

--- js/main.js
+++ js/main.js
@@ -1,79 +1,78 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.getElementById('novoItem');
+const lista = document.querySelector('#lista');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
 
 itens.forEach( (elemento) => {
-    criaElemento(elemento)
-} )
+  criaElemento(elemento)
+});
 
-form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+form.addEventListener('submit', (evento) => {
+  evento.preventDefault()
 
-    const existe = itens.find( elemento => elemento.nome === nome.value )
+  const nomeProduto = evento.target.elements['nome'];
+  const quantidadeProduto = evento.target.elements['quantidade'];
 
-    const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    }
 
-    if (existe) {
-        itemAtual.id = existe.id
-        
-        atualizaElemento(itemAtual)
+//VERIFICA PLURAL
+  if (nomeProduto.value.charAt(nomeProduto.value.length -1) != 's' && quantidadeProduto.value > 1){
+    definePlural(nomeProduto)
+  } 
+  
+//CRIA ITEM  
+  const item = {
+    'nome': nomeProduto.value,
+    'quantidade': quantidadeProduto.value
+  }
 
-        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
-    } else {
-        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+//VERIFICA ITENS REPETIDOS  
+  const existe = itens.find(elemento => elemento.nome === nomeProduto.value);
 
-        criaElemento(itemAtual)
+  if (existe){
+    item.id = existe.id;
+    atualizaElemento(item);
 
-        itens.push(itemAtual)
-    }
+    itens[existe.id] = item
+  }
+  else {
+    item.id = itens.length;
+    criaElemento(item);
+    itens.push(item) ;
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+  }
 
-    nome.value = ""
-    quantidade.value = ""
+  
+
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+
+  nomeProduto.value = '' 
+  quantidadeProduto.value = ''
+  
 })
 
-function criaElemento(item) {
-    const novoItem = document.createElement("li")
-    novoItem.classList.add("item")
 
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
-    novoItem.appendChild(numeroItem)
-    
-    novoItem.innerHTML += item.nome
 
-    novoItem.appendChild(botaoDeleta(item.id))
-
-    lista.appendChild(novoItem)
+function definePlural(nomeProduto){
+    return nomeProduto.value += 's'
 }
 
-function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
-}
+function criaElemento(item){
+  const novoItem = document.createElement('li');
+  novoItem.classList.add('item');
 
-function botaoDeleta(id) {
-    const elementoBotao = document.createElement("button")
-    elementoBotao.innerText = "X"
+  const numeroItem = document.createElement('strong');
+  numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
+  novoItem.appendChild(numeroItem)
 
-    elementoBotao.addEventListener("click", function() {
-        deletaElemento(this.parentNode, id)
-    })
+  novoItem.innerHTML += item.nome;
 
-    return elementoBotao
-}
+  lista.appendChild(novoItem)
 
-function deletaElemento(tag, id) {
-    tag.remove()
+} 
 
-    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
-
-    localStorage.setItem("itens", JSON.stringify(itens))
+function atualizaElemento (item){
+  document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
